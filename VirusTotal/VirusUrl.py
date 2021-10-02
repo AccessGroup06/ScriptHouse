7b52d8b7d72d77010c1493a489def291860275c5
AcessGroup <49107930+AccessGroup06@users.noreply.github.com>
2021-10-02 20:21:09 +0200

    Getting JSON Response

--- VirusTotal/VirusUrl.py
+++ VirusTotal/VirusUrl.py
@@ -1,7 +1,19 @@
-import requests,time,json
-#webSites=[
-#    "https://geekflare.com/website-malware-scanning/",
-#    "https://shodan.readthedocs.io/en/latest/examples/cert-stream.html",
-#    "https://sb.aau.dk/sb-ad/sb/","https://libgen.rs/"
-#]
-api_key = '57856d7e94768680c4d2378f9ecb8e4a2faa92889f55b42c487e5d1a982e5ab8'
+import requests
+import base64
+# Getting URL for Scannig
+
+get_url = input("Enter URL for Scanning... ")
+
+# Generating base 64 of the entered URL, required for vt api 
+url_id = base64.urlsafe_b64encode("{}".format(get_url).encode()).decode().strip("=")
+base64_Url = url_id
+
+# Api key for authentication with vt api
+api_key='57856d7e94768680c4d2378f9ecb8e4a2faa92889f55b42c487e5d1a982e5ab8'
+
+# Required header for the GET reqest
+headers = {'x-apikey': api_key}
+
+# Sending request plus appending the base64 of the URL
+r = requests.get("https://www.virustotal.com/api/v3/urls/{}".format(str(base64_Url)),headers=headers)
+print(r.text)
