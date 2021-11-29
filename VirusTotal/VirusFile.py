559ce62a636a457fb771af9bc49a20f891f16f40
Omar Hawwash <omarnabilhawwash@gmail.com>
2021-11-29 13:01:36 +0100

    Virus scan now filters KasperSky
    BUT: premium API key needed for more than one call per minute (so for scaling, this is needed)

--- VirusTotal/VirusFile.py
+++ VirusTotal/VirusFile.py
@@ -4,7 +4,7 @@ import requests, json, sys, os
 from requests.models import Response
 
 # The API key used by VirusTotal
-api_key = '57856d7e94768680c4d2378f9ecb8e4a2faa92889f55b42c487e5d1a982e5ab8'
+api_key = 'YOUR API KEY GOES HERE'
 
 # The URL needed for file submission and other formalia regarding VirusTotal
 url= "https://www.virustotal.com/api/v3/files"
@@ -35,10 +35,27 @@ print(urltoVT)
 r2 = requests.get(urltoVT,headers=headers)
 
 # Print the full JSON response, which includes the assessments from all the AV's
-print(r2.text)
+#print(r2.text)
+
+# Let's only get data from Kaspersky (or an AV of our choice) -- change the last part of the tuple if you want a different AV :) /OH
+ourAntivirus = jsonformatted["data"]["results"]["Kaspersky"]
+
+# Give us the category that our antivirus deems this to be:
+avfindingsCat = ourAntivirus["category"]
+
+# Give us the result from this AV:
+avfindingsResult = ourAntivirus["result"]
+
+# Give us the method result from this AV:
+avfindingsMethod = ourAntivirus["method"]
 
 # Parse the JSON response including the assessments into Python so we can retrieve the malicious stat later.
 jsonformatted = json.loads(r2.text)
+
+print("KasperSky has deemed this file to be:\n") # Remember to change the AV name in this hard-coded print, if needed :)
+print("Under the category: ", avfindingsCat)
+print("The result of the scan through this AV was: ", avfindingsResult)
+print("The method for this file scan is: ", avfindingsMethod)
 
 # How many times have we seen the word malicious in the full JSON response? (Retrieve malicious 'stat')
 flaggedMalicious = jsonformatted["data"]["attributes"]["stats"]["malicious"]
@@ -58,5 +75,5 @@ elif(1 > flaggedMalicious <= 3):
     print("The file may be malicious.")
 elif(flaggedMalicious >= 5):
     print("The file is most likely malicious.")
-else:
-    print("File may be corrupted or otherwise not readable. Please try again!") 
+#else:
+#    print("File may be corrupted or otherwise not readable. Please try again!") 
